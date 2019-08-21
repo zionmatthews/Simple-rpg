ab9db9e734f9a9cbca7750d727a3fe93ee108af9
zion matthews <zman5682@gmail.com>
2019-08-21 09:52:46 -0500

    Actually made somewhat of a game. I love and understand it.

--- Simple  rpg/Game.cs
+++ Simple  rpg/Game.cs
@@ -10,7 +10,39 @@ namespace Simple__rpg
     {
         public void Start()
         {
-            Console.Write("Welcome!");
+            string playerName = "";
+            int playerHealth = 100;
+
+            //Welcome the player
+            Console.Write("What is your name? ");
+            playerName = Console.ReadLine();
+            Console.WriteLine("Welcome, " + playerName + ".");
+
+            //Monster encounter!
+            int monsterDamage = 13;
+            Console.WriteLine("");
+            Console.WriteLine("There is a monster in front of you");
+
+            string action = "";
+            Console.Write("What will you do?(fight/flee) ");
+            action = Console.ReadLine();
+            if (action == "fight" || action == "Fight")
+            {
+                //monster attack
+                Console.WriteLine("The monster attacks!" + playerName + "takes " + monsterDamage + "damage!");
+                playerHealth = playerHealth - monsterDamage;
+                Console.WriteLine(playerName + " has " + playerHealth + " health remaining.");
+                //player attack
+                Console.WriteLine(playerName + " attack! The monster is defeated!");
+
+            }
+            else if (action == "flee" || action == "Flee")
+            {
+
+                //escape
+                Console.WriteLine("Got away safely...");
+            }
+
             Console.ReadKey();
         }
     }
