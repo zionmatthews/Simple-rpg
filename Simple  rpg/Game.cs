ace8efd287c6e86d72cb05947c48cf20d755d68d
zion matthews <zman5682@gmail.com>
2019-08-21 10:45:50 -0500

    Changes are great. love it

--- Simple  rpg/Game.cs
+++ Simple  rpg/Game.cs
@@ -8,30 +8,59 @@ namespace Simple__rpg
 {
     class Game
     {
+        string playerName = "";
+        int playerHealth = 100;
         public void Start()
         {
-            string playerName = "";
-            int playerHealth = 100;
 
+            Welcome();
+
+            int monsterRemaining = 5;
+            bool alive = true;
+
+           //fight until you lose
+            while (alive && monsterRemaining > 0)
+            {
+                Console.WriteLine("There are " + monsterRemaining + " monsters remaining.");
+                alive = Encounter(20);
+                monsterRemaining--;
+            }
+           
+            
+            //wait for user input before closing
+            Console.ReadKey();
+        }
+        void Welcome()
+        {
             //Welcome the player
             Console.Write("What is your name? ");
             playerName = Console.ReadLine();
             Console.WriteLine("Welcome, " + playerName + ".");
 
+        }
+
+        bool Encounter(int monsterDamage)
+        {
             //Monster encounter!
-            int monsterDamage = 13;
+            
             Console.WriteLine("");
             Console.WriteLine("There is a monster in front of you");
 
             string action = "";
-            Console.Write("What will you do?(fight/flee) ");
+            Console.Write("What will you do?(fight/flee) :o ");
             action = Console.ReadLine();
             if (action == "fight" || action == "Fight")
             {
                 //monster attack
-                Console.WriteLine("The monster attacks!" + playerName + "takes " + monsterDamage + "damage!");
+                Console.WriteLine("The monster attacks!" + playerName + " takes " + monsterDamage + " damage!");
                 playerHealth = playerHealth - monsterDamage;
                 Console.WriteLine(playerName + " has " + playerHealth + " health remaining.");
+                if (playerHealth <= 0)
+                {
+                    //player defeat!
+                    Console.WriteLine(playerName + " was defeated");
+                    return false;
+                }
                 //player attack
                 Console.WriteLine(playerName + " attack! The monster is defeated!");
 
@@ -40,10 +69,10 @@ namespace Simple__rpg
             {
 
                 //escape
-                Console.WriteLine("Got away safely...");
-            }
+                Console.WriteLine("You got away coward! ;(");
 
-            Console.ReadKey();
+            }
+            return true;
         }
     }
 }
