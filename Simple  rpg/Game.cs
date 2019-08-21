b4e4a14e135cd4bd5ea55744e937f965421f64b9
Zion Matthews <s198031@students.aie.edu>
2019-08-21 09:14:55 -0500

    Add project files.

---
+++ Simple  rpg/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple__rpg
+{
+    class Game
+    {
+        public void Start()
+        {
+            Console.Write("Welcome!");
+            Console.ReadKey();
+        }
+    }
+}
